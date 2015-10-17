7b6d7e10830eb8268e3754a885e7610ee611b383
JulianOrinyol <julianorinyol@gmail.com>
2015-10-17 16:22:30 -0700

    added pry and pry-nav to gemfile,  added helper functions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+  def restrict_access
+    if !current_user
+      flash[:alert] = "You must log in."
+      redirect_to new_session_path
+    end
+  end 
+   
+  def current_user
+    if session[:acting_as]
+      @current_user = User.find(session[:acting_as])
+    else
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+
+  helper_method :current_user
+  end
