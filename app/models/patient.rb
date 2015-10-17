b914b48eb39cb0bee570cbc5d42d10b47ee6c9f0
JulianOrinyol <julianorinyol@gmail.com>
2015-10-17 16:13:43 -0700

    added associations and validations on user, patient, appointment

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,9 @@
 class Patient < ActiveRecord::Base
+  has_many :appointments
+
+  validates :first_name, :last_name, :user_id, presence: true
+
+  def full_name
+      "#{first_name} #{last_name}"
+  end
 end
